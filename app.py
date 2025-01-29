6c14b93716c502162257cb46faa37f806f254361
Alan <asp17697@gmail.com>
2025-01-29 15:26:54 -0600

    update

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ app = App(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.ALLOWED_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
