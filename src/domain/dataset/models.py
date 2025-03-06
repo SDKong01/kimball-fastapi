c0ed5bc4293f47999eba2e41ab9407f875638fe4
Alan <asp17697@gmail.com>
2025-03-06 14:00:02 -0600

    Set as None some optional fields

--- src/domain/dataset/models.py
+++ src/domain/dataset/models.py
@@ -8,19 +8,19 @@ from dataclass_type_validator import dataclass_validate
 @dataclass(frozen=False)
 class Metadata:
     dataset_id: str
-    dataset_description: Optional[Dict[str, int]]
-    date_column: Optional[str]
-    date_grain: Optional[str]
     dataset_name: Optional[str]
-    target_columns: Optional[List[str]]
     dataset_type: Optional[str]
     data_source: Dict[str, str]
     default_forecast: Optional[int]
     owner: Optional[str]
-    tags: Optional[List[str]]
     description: Optional[str]
     is_cloned: Optional[bool]
     collection_name: Optional[str]
+    date_grain: Optional[str] = None
+    tags: Optional[List[str]] = None
+    dataset_description: Optional[Dict[str, int]] = None
+    target_columns: Optional[List[str]] = None
+    date_column: Optional[str] = None
     stats: Optional[Dict[str, Any]] = None
     is_active: Optional[bool] = True
     last_run: Optional[str] = None
