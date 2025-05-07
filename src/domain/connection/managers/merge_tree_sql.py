fb3569147d11e9adebb37359bc282a46459c44c9
Alan <asp17697@gmail.com>
2025-05-07 12:14:36 -0600

    update

--- src/domain/connection/managers/merge_tree_sql.py
+++ src/domain/connection/managers/merge_tree_sql.py
@@ -366,9 +366,8 @@ WHERE TABLE_NAME LIKE '%AUD%' OR TABLE_NAME LIKE '%TRAN%'"""
         if not rows_to_insert:
             return True
 
-        insert_statement = (
-            f"INSERT INTO {db}.{table} {f"({', '.join(fields)})"} VALUES "
-        )
+        parsed_fields = f"({', '.join(fields)})"
+        insert_statement = f"INSERT INTO {db}.{table} {parsed_fields} VALUES "
         for row in rows_to_insert:
             insert_statement += f"({', '.join([f"'{x}'" for x in row.values()])}), "
         insert_statement = insert_statement[:-2] + ";"
